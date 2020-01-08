9afa5e170983be876cfc971aea033dd39e7cdab3
KhosyAkmal <akmalkhosy@gmail.com>
2020-01-08 10:19:42 +0700

    replace name cfile and class

---
+++ lib/tab_bar/membership_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Membership extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.grey[800],
+        title: new Text("Membership"),
+      ),
+    );
+  }
+}
---
+++ lib/tab_bar/reward_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Reward extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.grey[800],
+        title: new Text("Reward"),
+      ),
+    );
+  }
+}
