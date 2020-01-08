0bd5c686ce7e1a95b60d51ec12f8f78cb4f4d823
KhosyAkmal <akmalkhosy@gmail.com>
2020-01-08 15:35:34 +0700

    add color

--- lib/shared/app_colors.dart
+++ lib/shared/app_colors.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
-const Color bar = Color(0xFF51C39A);
+const Color bar = Colors.white;
+const Color appbar = Color(0xFF252D47);
+const Color backgroundApp = Color(0xFFF5F7FA);
