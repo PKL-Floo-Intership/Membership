62fb618ba987612ad3074f182253911758bab199
KhosyAkmal <akmalkhosy@gmail.com>
2020-01-09 15:57:52 +0700

    change size text

--- lib/shared/text_style.dart
+++ lib/shared/text_style.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'app_colors.dart' as appColor;
 
 const textAppbar = TextStyle(fontWeight: FontWeight.w800, fontSize: 20.0, color: appColor.icon);
-const textTitleW = TextStyle(fontWeight: FontWeight.w500, fontSize: 22.0, color: appColor.backgroundApp);
+const textTitleW = TextStyle(fontWeight: FontWeight.w500, fontSize: 23.0, color: appColor.backgroundApp);
 const textTitleB = TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0, color: appColor.icon);
 const textCard = TextStyle(fontWeight: FontWeight.w200, fontSize: 20.0, color: appColor.backgroundApp);
+const textCardV = TextStyle(fontWeight: FontWeight.w200, fontSize: 16.0, color: appColor.backgroundApp);
