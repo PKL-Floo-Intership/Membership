d557a11f1c5059b152143bb1519648ccdb5d5515
TikaYulian27 <tikayulian27@gmail.com>
2020-01-09 14:18:51 +0700

    make forgot password

--- lib/login/my_login.dart
+++ lib/login/my_login.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:membership/forgot_password/forgot_page.dart';
 import 'package:membership/login/login_page.dart';
 import 'package:membership/tab_bar/promotion_view.dart';
 import 'package:membership/register/register_page.dart';
 import 'package:membership/main.dart';
+import 'package:membership/shared/app_colors.dart' as app_color;
 
 
 class MyLogin extends StatelessWidget {
-  
-  
-  
-  
   final routes = <String, WidgetBuilder>{
     LoginPage.toLogin: (context) => LoginPage(),
     Promotion.toPromotion: (context) => MyApp(),
     RegisterPage.toRegister: (context) => RegisterPage(),
+    ForgotPasswordPage.toForgotPassword: (context) => ForgotPasswordPage(),
   };
   @override
   Widget build(BuildContext context) {
