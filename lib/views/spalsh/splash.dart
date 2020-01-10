2a1760e314d0fd94db78179a05a56f5e6596d8d3
KhosyAkmal <akmalkhosy@gmail.com>
2020-01-10 10:37:56 +0700

    add dart:async

---
+++ lib/views/spalsh/splash.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:membership/views/login/login_page.dart';
+import 'package:membership/views/login/my_login.dart';
+import 'dart:async';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState(){
+    super.initState();
+    Future.delayed(Duration(
+      seconds: 1
+      ),() {
+        Navigator.push(
+          context, 
+          MaterialPageRoute(
+            builder: (context) => MyLogin(),
+          ),
+        );
+      },
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+         body: Center(
+           child : new Image.asset(
+             "assets/wel.png",
+             ),
+         ),
+    );
+  }
+}
