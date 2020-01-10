04f14418a4bed3f3e257227d424d791c63b55e8b
KhosyAkmal <akmalkhosy@gmail.com>
2020-01-10 09:48:56 +0700

    change color & add card

--- lib/views/promotion/promotion_view.dart
+++ lib/views/promotion/promotion_view.dart
@@ -3,12 +3,11 @@ import 'package:membership/shared/app_colors.dart' as app_color;
 import 'package:membership/shared/text_style.dart' as textStyle;
 
 class Promotion extends StatelessWidget {
-
   static String toPromotion = 'promotion-page';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: app_color.backgroundApp,
+      backgroundColor: app_color.backgroundApp,
       appBar: new AppBar(
         backgroundColor: app_color.bar,
         title: new Text("Promotion"),
@@ -20,19 +19,19 @@ class Promotion extends StatelessWidget {
             promotionTitle: "Promotion Title",
             decs: "Test test test test test \nTest test test test test \nTest test test test test",
             expire: "Expire : 22 Juli 2019",
-            ),
-            new ListPromotion(
+          ),
+          new ListPromotion(
             photo: "assets/logo.png",
             promotionTitle: "Promotion Title",
-            decs: "Test test test test test \nTest test test test test \nTest test test test test",
+            decs:"Test test test test test \nTest test test test test \nTest test test test test",
             expire: "Expire : 22 Juli 2019",
-            ),
-            new ListPromotion(
+          ),
+          new ListPromotion(
             photo: "assets/logo.png",
             promotionTitle: "Promotion Title",
-            decs: "Test test test test test \nTest test test test test \nTest test test test test ",
+            decs:"Test test test test test \nTest test test test test \nTest test test test test ",
             expire: "Expire : 22 Juli 2019",
-            ),
+          ),
         ],
       ),
     );
@@ -40,37 +39,46 @@ class Promotion extends StatelessWidget {
 }
 
 class ListPromotion extends StatelessWidget {
-
-  ListPromotion({this.photo, this.promotionTitle, this. decs, this.expire});
+  ListPromotion({this.photo, this.promotionTitle, this.decs, this.expire});
 
   final String photo;
   final String promotionTitle;
   final String decs;
   final String expire;
-  
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: new EdgeInsets.all(20),
+      padding: new EdgeInsets.all(10),
       child: new Center(
-        child: Row(
-          children: <Widget>[
-            new Image.asset(photo, width: 120,),
-            
-            new Container(
-              padding: EdgeInsets.all(20),
-              child: new Center(
-                child: new Column(
-                    children: <Widget>[
-                      new Text(promotionTitle,style: textStyle.textListT),
-                      new Text(decs, style: textStyle.textListD),
-                      new Text(expire, style: textStyle.textListx),
-                    ],
+        child: new Padding(
+          padding: const EdgeInsets.all(5),
+          child: Card(
+            color: app_color.bar,
+            child: new Column(
+              children: <Widget>[
+                new Row(
+                  children: <Widget>[
+                    new Image.asset(
+                      photo,
+                      width: 120,
+                    ),
+                    new Container(
+                      padding: EdgeInsets.all(15),
+                      child: new Center(
+                          child: new Column(
+                        children: <Widget>[
+                          new Text(promotionTitle, style: textStyle.textListT),
+                          new Text(decs, style: textStyle.textListD),
+                          new Text(expire, style: textStyle.textListx),
+                        ],
+                      )),
+                    )
+                  ],
                 )
-              ),
-            )
-          ],
+              ],
+            ),
+          ),
         ),
       ),
     );
