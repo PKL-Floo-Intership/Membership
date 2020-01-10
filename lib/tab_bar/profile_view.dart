2408c25e9bb155c95b10ddf449d04ece8a36f283
TikaYulian27 <tikayulian27@gmail.com>
2020-01-10 08:46:30 +0700

    change hint confirm password

--- lib/tab_bar/profile_view.dart
+++ lib/tab_bar/profile_view.dart
@@ -1,15 +1,159 @@
 import 'package:flutter/material.dart';
 import 'package:membership/shared/app_colors.dart' as app_color;
 
-class Profile extends StatelessWidget {
+
+class Profile extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    
+    final username = TextFormField(
+      keyboardType: TextInputType.text,
+      autofocus: false,
+      initialValue: '',
+      decoration: InputDecoration(
+        hintText: 'John Legend',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      initialValue: '',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final confirmPassword = TextFormField(
+      autofocus: false,
+      initialValue: '',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Confirm Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final birthday = TextFormField(
+      keyboardType: TextInputType.datetime,
+      autofocus: false,
+      initialValue: '',
+      decoration: InputDecoration(
+        hintText: 'dd/mm/yy',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final namaDepan = TextFormField(
+      keyboardType: TextInputType.text,
+      autofocus: false,
+      initialValue: '',
+      decoration: InputDecoration(
+        hintText: 'John',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final namaBelakang = TextFormField(
+      keyboardType: TextInputType.text,
+      autofocus: false,
+      initialValue: '',
+      decoration: InputDecoration(
+        hintText: 'Legend',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final noHP = TextFormField(
+      keyboardType: TextInputType.phone,
+      autofocus: false,
+      initialValue: '',
+      decoration: InputDecoration(
+        hintText: '08xxxxxxx',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final idNumber = TextFormField(
+      keyboardType: TextInputType.number,
+      autofocus: false,
+      initialValue: '',
+      decoration: InputDecoration(
+        hintText: 'John Legend',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final alamat = TextFormField(
+      keyboardType: TextInputType.text,
+      autofocus: false,
+      initialValue: '',
+      decoration: InputDecoration(
+        hintText: 'Jl. Sendiri',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0),),
+      ),
+    );
+
+    final saveButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {
+          // Navigator.of(context).pushNamed(HomePage.tag);
+        },
+        padding: EdgeInsets.all(12),
+        color: app_color.icon,
+        child: Text('Save', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
     return Scaffold(
       backgroundColor: app_color.backgroundApp,
       appBar: new AppBar(
         backgroundColor: app_color.bar,
         title: new Text("Profile Details"),
       ),
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0, top: 30),
+          children: <Widget>[
+            username,
+            SizedBox(height: 15.0),
+            password,
+            SizedBox(height: 15.0),
+            confirmPassword,
+            SizedBox(height: 15.0),
+            birthday,
+            SizedBox(height: 15.0),
+            namaDepan,
+            SizedBox(height: 15.0),
+            namaBelakang,
+            SizedBox(height: 15.0),
+            noHP,
+            SizedBox(height: 15.0),
+            alamat,
+            SizedBox(height: 15.0),
+            saveButton
+          ],
+        ),
+      ),
     );
   }
 }
+
+
+
