fac68612ca855d20cbabbc1769b6236083479ad4
TikaYulian27 <tikayulian27@gmail.com>
2020-01-14 13:46:32 +0700

    add color and text style

--- lib/shared/app_colors.dart
+++ lib/shared/app_colors.dart
@@ -4,3 +4,6 @@ const Color bar = Colors.white;
 const Color appbar = Color(0xFF252D47);
 const Color backgroundApp = Color(0xFFF5F7FA);
 const Color icon = Color(0xFF252D47);
+const Color inputError = Color(0xFFFF5252);
+const Color inputBorderLine = Color(0xFFEDF1F6);
+const Color buttonDefault = Color(0xFF238AFF);
